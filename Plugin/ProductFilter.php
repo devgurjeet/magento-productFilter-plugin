10e743e6cff382201ac8e866cc025392be6282bc
Gurjeet Singh <developer.gurjeet@gmail.com>
2016-07-01 21:22:47 +0530

    add functions

--- Plugin/ProductFilter.php
+++ Plugin/ProductFilter.php
@@ -2,12 +2,21 @@
 namespace GSS\ProductFilter\Plugin;
 
 class ProductFilter{
+
 	public function afterGetName(\Magento\Catalog\Model\Product $subject, $result) {
 	        return '[' . $result . ']';
     }
 
     public function afterGetPrice(\Magento\Catalog\Model\Product $subject, $result) {
-       	return $result*0;
+       	return 3;
     }
+
+    public function afterIsSaleable(\Magento\Catalog\Model\Product $subject, $result) {
+       	return 0;
+    }
+
+    public function afterIsAvailable(\Magento\Catalog\Model\Product $subject, $result) {
+       	return 0;
+    }   
     
 }
